8b2246920f75abf72ce48019540d3f894ad928c7
Gevorg-Khachatryaan <Gevorg1050.1>
2021-09-02 17:23:45 +0300

    test cases

--- ssn_main/models.py
+++ ssn_main/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
 
 
 class CustomUser(User):
@@ -11,9 +10,15 @@ class CustomUser(User):
     def __str__(self):
         return self.username
 
+    class Meta:
+        ordering = ['pk']
+
 
 class Post(models.Model):
     title = models.CharField('Title', max_length=32)
     content = models.TextField('Content')
     creator = models.OneToOneField('CustomUser', related_name='created_by_%(class)s_related', on_delete=models.CASCADE)
     like = models.ManyToManyField(CustomUser, null=True, blank=True)
+
+    class Meta:
+        ordering = ['pk']
